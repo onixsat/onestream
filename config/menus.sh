710e028d94c5592803e871eebe9bb04f64b33cbe
onixsat <163943278+onixsat@users.noreply.github.com>
2026-07-07 11:39:27 -0700

    Update menu items and source files in menus.sh

--- config/menus.sh
+++ config/menus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-HEADER_MSG="${CYAN}Bash Ubuntu${NORMAL}"
+HEADER_MSG="${CYAN}OS Gestor${NORMAL}"
 
 read -r -d '' ENV_VAR_MENU << EOM
 ${RED}Main Menu${NORMAL}
@@ -7,9 +7,9 @@ EOM
 createMenu "mainMenu" "$ENV_VAR_MENU"
 addMenuItem "mainMenu" "Servidor" loadMenu "menuServidor"
 addMenuItem "mainMenu" "Nginx" loadMenu "menuNginx"
-addMenuItem "mainMenu" "DNS" loadMenu "menuDns"
+addMenuItem "mainMenu" "Alertas" loadMenu "menuAlertas"
 addMenuItem "mainMenu" "Quit" l8r
 
 source menus/servidor.sh
 source menus/nginx.sh
-source menus/dns.sh
+source menus/alertas.sh
